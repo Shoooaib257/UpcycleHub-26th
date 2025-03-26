719c3daf4233f18a0ce509fbde5996f27230c080
Shoooaib257 <shoaibdpsblr@gmail.com>
2025-03-27 02:50:16 +0530

    Update Netlify function configuration to properly bundle dependencies

---
+++ netlify/functions/build.js
@@ -0,0 +1,10 @@
+const esbuild = require('esbuild');
+
+esbuild.build({
+  entryPoints: ['api-direct.js'],
+  bundle: true,
+  minify: true,
+  platform: 'node',
+  target: 'node14',
+  outfile: 'api-direct.bundled.js',
+}).catch(() => process.exit(1)); 
